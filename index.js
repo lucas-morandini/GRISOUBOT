7f062f5cc917247635703033f390f5d20914ee30
lucas-morandini <lucas.morandini@etu.umontpellier.fr>
2022-11-26 18:42:42 +0100

    fix : colors

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 ﻿//Importing all needed Commands
 const Discord = require("discord.js"); //this is the official discord.js wrapper for the Discord Api, which we use!
-const colors = require("colors"); //this Package is used, to change the colors of our Console! (optional and doesnt effect performance)
 const fs = require("fs"); //this package is for reading files and getting their inputs
 
 //Creating the Discord.js Client for This Bot with some default settings ;) and with partials, so you can fetch OLD messages
